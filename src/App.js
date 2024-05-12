73e66d036d835b335d363447e16c15ac3b1a8853
Itoshi1 <hyolmosonamtshering@gmail.com>
2024-05-12 21:36:50 +0545

    changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react'
-import Home from './features/home/Home'
 
 const App = () => {
 
+
+  const handleAlert = () => {
+    alert('don\'t copy');
+  }
+
+  const eventCall = (e, msg) => {
+    console.log(e);
+    alert(msg);
+  }
+
+
+  window.addEventListener('click', (e) => {
+    console.log('hello jeee');
+  });
+
   return (
-    <div>
-      <Home />
+    <div className='p-4'>
+      <button onClick={() => eventCall('hello jee')} className='bg-slate-600 text-white p-2 rounded' >Click Me</button>
+
+      <p > <span onCopy={handleAlert}>Lorem </span> ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sunt voluptatum alias molestiae, repellendus ad? Alias culpa recusandae architecto natus.</p>
     </div>
   )
 }
