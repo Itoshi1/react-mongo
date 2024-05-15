4770cd1445a93b9bbba8f433de32da39bc29eaca
Itoshi1 <hyolmosonamtshering@gmail.com>
2024-05-15 09:04:06 +0545

    change

--- src/features/home/Tech.js
+++ src/features/home/Tech.js
@@ -5,7 +5,7 @@ const Tech = () => {
   return (
     <div className='my-6'>
       <h1 className='text-center text-2xl'>Technologies I use</h1>
-      <div className='grid grid-cols-4 justify-items-center grid-cols-autoFit'>
+      <div className='grid justify-items-center grid-cols-autoFit'>
         <DiReact size={200} className='hover:scale-110 hover:text-pink-700 transition-all ease-in' />
         <DiPython size={200} />
         <DiCss3 size={200} />
